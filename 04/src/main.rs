380b29e720c81771bc18e5b87d103cdff0c4801e
Stephan Pernkopf <stephan@pernkopf.dev>
2020-12-04 13:44:54 +0100

    Add day_4 part 1

---
+++ 04/src/main.rs
@@ -0,0 +1,155 @@
+use std::path::Path;
+use std::str::FromStr;
+use std::{
+    collections::HashMap,
+    io::{self, BufRead},
+};
+use std::{fmt::Debug, fs::File};
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn get_input<T>(file: &str) -> Vec<T>
+where
+    T: FromStr,
+    <T as FromStr>::Err: Debug,
+{
+    // unwrap is acceptable because of known input
+    read_lines(file)
+        .unwrap()
+        .map(|line| line.unwrap().parse::<T>().unwrap())
+        .collect::<Vec<_>>()
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+enum FieldType {
+    BirthYear,
+    IssueYear,
+    ExpirationYear,
+    Height,
+    HairColor,
+    EyeColor,
+    PassportID,
+    CountryID,
+}
+
+impl FromStr for FieldType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let field_type = match s {
+            "byr" => FieldType::BirthYear,
+            "iyr" => FieldType::IssueYear,
+            "eyr" => FieldType::ExpirationYear,
+            "hgt" => FieldType::Height,
+            "hcl" => FieldType::HairColor,
+            "ecl" => FieldType::EyeColor,
+            "pid" => FieldType::PassportID,
+            "cid" => FieldType::CountryID,
+            _ => return Err(()),
+        };
+
+        Ok(field_type)
+    }
+}
+
+struct PassportField {
+    field_type: FieldType,
+    field: String,
+}
+
+impl PassportField {
+    pub fn new(field_type: &str, field: &str) -> PassportField {
+        if field_type.is_empty() || field.is_empty() {
+            panic!("empty field_type '{}' or field '{}'", field_type, field)
+        }
+        PassportField {
+            field_type: FieldType::from_str(field_type).expect("Couldn't parse field string"),
+            field: String::from(field),
+        }
+    }
+}
+
+const NEEDED_FIELDS: &'static [FieldType] = &[
+    FieldType::BirthYear,
+    //FieldType::CountryID,
+    FieldType::ExpirationYear,
+    FieldType::EyeColor,
+    FieldType::HairColor,
+    FieldType::Height,
+    FieldType::IssueYear,
+    FieldType::PassportID
+];
+
+struct Passport {
+    pub fields: HashMap<FieldType, PassportField>,
+}
+
+impl Passport {
+    pub fn new(input_fields: Vec<PassportField>) -> Passport {
+        let mut fields: HashMap<FieldType, PassportField> = HashMap::with_capacity(input_fields.len());
+
+        for f in input_fields {
+            fields.insert(f.field_type, f);
+        }
+
+        Passport {
+            fields,
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        for key in NEEDED_FIELDS {
+            if !self.fields.contains_key(key) {
+                return false
+            }
+        }
+
+        true
+    }
+}
+
+fn parse_input(input: Vec<String>) -> Vec<Passport> {
+    let mut result: Vec<Passport> = Vec::new();
+    let mut curr_fields: Vec<PassportField> = Vec::new();
+
+    for line in input {
+        if line.is_empty() {
+            result.push(Passport::new(curr_fields));
+            curr_fields = Vec::new();
+
+            continue;
+        }
+
+        let fields = line
+            .split_whitespace()
+            .map(|f| {
+                let split = f.splitn(2, ':').collect::<Vec<_>>();
+                (split[0], split[1])
+            })
+            .map(|t| PassportField::new(t.0, t.1));
+
+        curr_fields.extend(fields);
+    }
+
+    if !curr_fields.is_empty() {
+        result.push(Passport::new(curr_fields));
+    }
+
+    result
+}
+
+fn main() {
+    let input = get_input::<String>("./input");
+
+    let passports = parse_input(input);
+
+    let result = passports.iter().filter(|p| p.is_valid()).count();
+
+    println!("RESULT: {}", result);
+}
