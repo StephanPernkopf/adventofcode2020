dde77e74812f38e3438cd7f5e4f5f44d5bf089e4
Stephan Pernkopf <stephan@pernkopf.dev>
2020-12-06 20:23:32 +0100

    Solve day_06 part 1

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -1,9 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-}
-use std::io::{self, BufRead};
+use std::iter::FromIterator;
 use std::path::Path;
 use std::str::FromStr;
+use std::{
+    collections::HashSet,
+    io::{self, BufRead},
+};
 use std::{fmt::Debug, fs::File};
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -24,4 +25,52 @@ where
         .unwrap()
         .map(|line| line.unwrap().parse::<T>().unwrap())
         .collect::<Vec<_>>()
+}
+
+#[derive(Debug)]
+struct GroupAnswer {
+    answers: HashSet<char>,
+}
+
+impl GroupAnswer {
+    fn new(answers_with_duplicates: Vec<char>) -> GroupAnswer {
+        GroupAnswer {
+            answers: HashSet::from_iter(answers_with_duplicates.into_iter()),
+        }
+    }
+
+    fn get_total_answers(&self) -> usize {
+        self.answers.len()
+    }
+}
+
+fn parse_input(input: Vec<String>) -> Vec<GroupAnswer> {
+    let mut group_answers: Vec<GroupAnswer> = Vec::new();
+    let mut curr_answers: Vec<char> = Vec::new();
+
+    for line in input {
+        if line.is_empty() {
+            group_answers.push(GroupAnswer::new(curr_answers));
+            curr_answers = Vec::new();
+        }
+        curr_answers.extend(line.chars());
+    }
+
+    if !curr_answers.is_empty() {
+        group_answers.push(GroupAnswer::new(curr_answers));
+    }
+
+    group_answers
+}
+
+fn main() {
+    let input: Vec<String> = get_input("./input");
+    let group_answers = parse_input(input);
+
+    let result: usize = group_answers
+        .iter()
+        .map(GroupAnswer::get_total_answers)
+        .sum();
+
+    println!("Result: {}", result);
 }
